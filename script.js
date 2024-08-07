ad69bc9e240226e13b01581bc8d937126314a508
Makaka449 <145019010+Makaka449@users.noreply.github.com>
2024-08-07 21:43:35 +0300

    last verchion 1.1

--- script.js
+++
@@ -1,6 +0,0 @@
-function showMore(islandId) {
-    const island = document.getElementById(islandId);
-    const moreText = document.createElement('p');
-    moreText.textContent = 'Дополнительная информация о ' + island.querySelector('h2').textContent + '.';
-    island.querySelector('.content').appendChild(moreText);
-}
